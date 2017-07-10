872e440a3ad316e401e006eb465ee750fdfa5bdf
Yana Shcherba <pani_yanka@mail.ru>
2017-07-10 17:25:12 +0300

    1

--- container/app.jsx
+++ container/app.jsx
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 
 const arr = [{name: 'Kek', age: '30'}, {name: 'Kotovski', age: '23'}, {name: 'Gowniana Marionetka', age: '18'}];
 
+constructor(props) {
+    super(props);
 
-class App extends Component {
-  const mapped = this.props.data.map(function(data, idx) {
-    return ([
-      <p key={idx}>{mapped.name}></p>,
-      <p key={idx}>{mapped.age}></p>,
-    ]);
-  });
+    this.state = {
+        name: arr.name(),
+        age: arr.age({ full: true })
+    };
+}
+
+const mapped = this.state.data.map(function(data, idx) {
+  return ([
+    <p key={idx}>{mapped.name}></p>,
+    <p key={idx}>{mapped.age}></p>,
+  ]);
+});
+
+
+class App extends React.Component {
   render() {
     return (
       <div>
       {mapped}
       </div>,
-      document.getElementById('one')
     );
   }
 
